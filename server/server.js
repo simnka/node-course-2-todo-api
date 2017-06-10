ef50c53ef289a07a71ea9c9337e0993adfc11651
Simon Nkangi <simon.swn@gmail.com>
2017-06-10 21:41:22 +0200

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
 
 var app = express();
 
@@ -26,6 +28,28 @@ app.get('/todos', (req, res) => {
         res.send({todos})
     }, (e) => {
         res.status(400).send(e);
+    });
+});
+
+// GET /todos/1234324
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    // Validate ID using isValid
+        // 404 - send back empty send
+
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if(!todo){
+            return res.status(404).send();
+        }
+        res.send({todo});
+    },(e) => {
+        res.status(400).send();
     });
 });
 
